d272c84fb54ce79ec46eac9f7ee0e9ad6b73a6be
Mohit Goyal <mohitgoyal1108@gmail.com>
2023-06-27 18:21:24 +0530

    Added About us Script

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
 import HeroSection from "../Components/Sections/HeroSection";
-
+import About from "../Components/Sections/about";
 export default function Home() {
   return (
     <div className="">
       <HeroSection></HeroSection>
+      <About></About>
     </div>
   );
 }

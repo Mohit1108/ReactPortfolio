5ab1aea1b0f1e2f46fb56afd3b95987f5eec047c
Mohit Goyal <mohitgoyal1108@gmail.com>
2023-07-15 17:37:28 +0530

    UI Enhance

--- src/Components/Sections/ProjectTabs.js
+++ src/Components/Sections/ProjectTabs.js
@@ -35,13 +35,8 @@ const ProjectTabs = () => {
 
   return (
     <div className="MySection " id="projects">
-<<<<<<< HEAD
       <div className="row tabsCategory border-top border-end border-bottom">
         <div className="col-md-2 m-0 p-0 border-end">
-=======
-      <div className=" tabsCategory border-top border-end border-bottom">
-        <div className="col-md-2 m-0 p-0 border-end catList">
->>>>>>> d4cd88a8426c3cdf8bd6c93ce5ccb73a7fb8aa8e
           <div className="border-bottom  ">
             <h3 className="p-2 m-0 small border-end">_categories_</h3>
           </div>
@@ -55,11 +50,7 @@ const ProjectTabs = () => {
                 }`}
               >
                 <label className="form-check-label d-flex justify-content-between">
-<<<<<<< HEAD
                   <div className="d-flex justify-content-start">
-=======
-                  <div className="d-flex justify-content-start align-items-center">
->>>>>>> d4cd88a8426c3cdf8bd6c93ce5ccb73a7fb8aa8e
                     <input
                       type="checkbox"
                       className="form-check-input"
